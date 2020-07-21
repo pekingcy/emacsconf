daff7cb26cdb4b04fb069110630f3432dccefd13
柴志勇 <jinju@7lk.com>
2020-07-21 22:13:13 +0800

    modified

--- init.el
+++ init.el
@@ -7,6 +7,11 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
+(require 'org-install)
+(require 'ob-tangle)
+(org-babel-load-file (expand-file-name "pekingcy.org" user-emacs-directory))
+
+
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 
 ;; 快速打开配置文件
