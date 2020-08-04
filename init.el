6bd4ff7158b777308c09b509fed31afb8cd3e7e3
chaizhiyong <703501246@qq.com>
2020-08-04 21:08:27 +0800

    org

--- init.el
+++ init.el
@@ -8,6 +8,26 @@
 
 (package-initialize)
 
-(require 'org-install)
-(require 'ob-tangle)
-(org-babel-load-file (expand-file-name "pekingcy.org" user-emacs-directory))
+;;(require 'cask "~/.cask/cask.el")
+;;(cask-initialize)    ; 类似于 package-initialize
+;;(require 'pallet)
+;;(pallet-mode t)      ; 激活 pallet, 在安装包时将 Cask 文件写入相应信息(require 'org-install)
+
+;;(require 'ob-tangle)
+;;(org-babel-load-file (expand-file-name "pekingcy.org" user-emacs-directory))
+
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+
+;; 快速打开配置文件
+(defun open-init-file()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+
+(require 'init-packages)
+(require 'init-ui)
+(require 'init-org)
+(require 'init-better-defaults)
+(require 'init-keybindings)
+(setq custom-file (expand-file-name "lisp/custom.el" user-emacs-directory))
+
+(load-file custom-file)
